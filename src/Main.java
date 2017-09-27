ffeb91027b29e38128ab9d0d89989fab80579a1e
Primo093021 <patty08217277@gmail.com>
2017-09-27 16:38:06 +0800

    modified

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("106024008\n資傳系\n1B\n黃瑜珮");
+    System.out.println("\\");
+    System.out.print("\"");
+    System.out.println("This is a test\tfor the project");
+    }
+}
